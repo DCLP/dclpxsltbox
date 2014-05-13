7ce8274d32ca272c70d6bd9dae3867cac6a0c209
Tom Elliott <tom.elliott@nyu.edu>
2014-05-13 17:03:27 -0500

    updated command-line transform (windows) to deal with new working directory structures

--- bin/2html_window.bat
+++ bin/2html_window.bat
@@ -1,2 +1,2 @@
 IF [%1]==[] ( echo ayuu nai
-) ELSE (java -jar %1\bin\claxon-1.0.0-SNAPSHOT-standalone.jar --xsl %1\xslt\pn-xslt\RDF2HTML.xsl --dir %1\data\ --out %1\output\dclp\ --ext .html collection dclp analytics no cssbase '..\..\css' jsbase '..\..\js')
+) ELSE (java -jar %1\dclpxsltbox\bin\claxon-1.0.0-SNAPSHOT-standalone.jar --xsl %1\navigator\pn-xslt\MakeHTML.xsl --dir %1\idp.data\DCLP\ --out %1\dclpxsltbox\output\dclp\ --ext .html collection dclp analytics no cssbase '..\..\css' jsbase '..\..\js')
