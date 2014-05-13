16a866a27b085a9ec46eaed825f3c0514f6cbc4e
Tom Elliott <tom.elliott@nyu.edu>
2014-05-13 17:01:34 -0500

    update command-line transform to deal with new working directory structures

--- bin/2html.sh
+++ bin/2html.sh
@@ -2,7 +2,7 @@
 
 if [ "$#" -ne 1 ]
 then
-  echo "Usage: 2html {path/to/dclptestbox}"
+  echo "Usage: 2html {path/to/working/directory/that/contains/dclpxsltbox/}"
   exit 1
 fi
-java -jar $1/bin/claxon-1.0.0-SNAPSHOT-standalone.jar --xsl $1/xslt/pn-xslt/RDF2HTML.xsl --dir $1/data/ --out $1/output/dclp/ --ext .html collection dclp analytics no cssbase '../../css' jsbase '../../js'
+java -jar $1/dclpxsltbox/bin/claxon-1.0.0-SNAPSHOT-standalone.jar --xsl $1/navigator/pn-xslt/MakeHTML.xsl --dir $1/idp.data/DCLP/ --out $1/dclpxsltbox/output/dclp/ --ext .html collection dclp analytics no cssbase '../../css' jsbase '../../js'
