e6f2c82b2651af662cd436fc45e42545eecae42d
Tom Elliott <tom.elliott@nyu.edu>
2015-04-30 10:26:49 -0500

    modify bulk transform script to pass appropriate, local-system-specific path to directory containing idp.data (assuming dclp working development environment and test harness as laid out in README)

--- bin/2html.sh
+++ bin/2html.sh
@@ -1,8 +1,12 @@
 #! /bin/bash
+set -x #echo on
+
 
 if [ "$#" -ne 1 ]
 then
   echo "Usage: 2html {path/to/working/directory/that/contains/dclpxsltbox/}"
   exit 1
 fi
-java -jar $1/dclpxsltbox/bin/claxon-1.0.0-SNAPSHOT-standalone.jar --xsl $1/navigator/pn-xslt/MakeHTML.xsl --dir $1/idp.data/DCLP/ --out $1/dclpxsltbox/output/dclp/ --ext .html collection dclp analytics no cssbase '../../css' jsbase '../../js'
+abspath=$(unset CDPATH && cd "$(dirname "$1")" && echo $PWD/$(basename "$1"))
+echo $abspath
+java -jar $1/dclpxsltbox/bin/claxon-1.0.0-SNAPSHOT-standalone.jar --xsl $1/navigator/pn-xslt/MakeHTML.xsl --dir $1/idp.data/DCLP/ --out $1/dclpxsltbox/output/dclp/ --ext .html collection dclp analytics no cssbase '../../css' jsbase '../../js' path "$abspath/idp.data"
