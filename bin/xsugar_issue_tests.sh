475b9c293501cfeaf48f59f25541e01f3eeabd86
Tom Elliott <tom.elliott@nyu.edu>
2017-05-25 16:34:41 -0500

    added xsugar coverage testing framework for issues along with test for #149

---
+++ bin/xsugar_issue_tests.sh
@@ -0,0 +1,27 @@
+#! /bin/bash
+set -o pipefail  # trace ERR through pipes
+set -o errtrace  # trace ERR through 'time command' and other functions
+function error() {
+    JOB="$0"              # job name
+    LASTLINE="$1"         # line of error occurrence
+    LASTERR="$2"          # error code
+    echo "ERROR in ${JOB} : line ${LASTLINE} with exit code ${LASTERR}"
+    exit 1
+}
+trap 'error ${LINENO} ${?}' ERR
+
+if (( $#==1 )); then
+	cd $1
+elif (( $#>1 )); then
+	echo "too many arguments; expected 1"
+	exit
+elif (( $#<1 )); then
+	echo "no top-level directory specified"
+	echo "usage: xsugar_issue_tests.sh absolute/or/relative/path/to/top/level/working/directory"
+	exit
+fi
+
+set -x #echo on
+
+cd xsugar
+jruby -Xcompat.version=1.8 -S bundle exec rake coverage:ddb DDB_DATA_PATH=../dclpxsltbox/tests/xsugar/ SAMPLE_FRAGMENTS=0 HTML_OUTPUT=../dclpxsltbox/output/xsugar/issue-tests-coverage.html 2>&1 | tee -a ../dclpxsltbox/output/xsugar/issue-tests-coverage-log.txt
